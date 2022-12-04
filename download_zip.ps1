7881cfa439fd59e3d4e0cc6a89367782ecc79903
Vitalii Bartashchuk <bartvitaly@cloud.upwork.com>
2022-12-04 18:40:58 +0100

    first commit

---
+++ download_zip.ps1
@@ -0,0 +1,13 @@
+﻿#$source = 'http://speedtest.tele2.net/10MB.zip'
+# Destination to save the file
+$destination = 'c:\10MB.zip'
+$outpath = 'c:\10MB'
+#Download the file
+#Invoke-WebRequest -Uri $source -OutFile $destination
+
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+[System.IO.Compression.ZipFile]::ExtractToDirectory($destination, $outpath)
+
+#Expand-Archive C:\10MB.zip -DestinationPath C:\10MB
